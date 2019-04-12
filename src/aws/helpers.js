443b7476e699696a49be5dde5e460d182d70baba
Matt Turner <matt@fiverings.uk>
2019-04-12 17:41:09 +0100

    Reduce the callback payload size

--- src/aws/helpers.js
+++ src/aws/helpers.js
@@ -11,35 +11,12 @@ const turnOffCallbackPayload = (userId, duration) => {
 
         'request': {
             'type': 'IntentRequest',
-            'requestId': process.env.ALEXA_REQUEST_ID,
-            'timestamp': new Date().toISOString(),
-            'locale': 'en-GB',
             'intent': {
                 'name': 'TurnIntent',
-                'confirmationStatus': 'NONE',
                 'slots': {
                     'onoff': {
                         'name': 'onoff',
-                        'value': 'off',
-                        'resolutions': {
-                            'resolutionsPerAuthority': [
-                                {
-                                    'authority': process.env.ALEXA_AUTHORITY,
-                                    'status': {
-                                        'code': 'ER_SUCCESS_MATCH'
-                                    },
-                                    'values': [
-                                        {
-                                            'value': {
-                                                'name': 'off',
-                                                'id': process.env.ALEXA_OFF_VALUE_ID
-                                            }
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                        'confirmationStatus': 'NONE'
+                        'value': 'off'
                     }
                 }
             }
