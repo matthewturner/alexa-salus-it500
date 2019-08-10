b5bf929d9d2cb18ed825fdc1ce70fc1a90363cac
Matt Turner <matt@fiverings.uk>
2019-08-10 21:17:37 +0100

    Add sample discovery

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-aws lambda update-function-code --function-name Salus --zip-file fileb://scripts/package.zip --profile salus
-aws lambda update-function-code --function-name smartheat-google-assistant --zip-file fileb://scripts/package.zip --profile salus
+# aws lambda update-function-code --function-name Salus --zip-file fileb://scripts/package.zip --profile salus
+# aws lambda update-function-code --function-name smartheat-google-assistant --zip-file fileb://scripts/package.zip --profile salus
+
+aws lambda update-function-code --function-name SmartHeat --zip-file fileb://scripts/package.zip --profile salus
---
+++ src/aws/smarthome-lambda.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// const DynamodbThermostatRepository = require('./ThermostatRepository');
+// const DefaultThermostatRepository = require('../core/ThermostatRepository');
+// const AwsHoldStrategy = require('./HoldStrategy');
+// const DefaultHoldStrategy = require('../core/HoldStrategy');
+// const ControlService = require('../core/ControlService');
+const Logger = require('../core/Logger');
+// const helpers = require('./helpers');
+// const Factory = require('../thermostats/Factory');
+const AlexaResponse = require('./AlexaResponse');
+
+const logger = new Logger(process.env.LOG_LEVEL || Logger.DEBUG);
+
+exports.handler = async (event, context) => {
+    logger.debug('index.handler request  -----');
+    logger.debug(JSON.stringify(event));
+
+    if (context !== undefined) {
+        logger.debug('index.handler context  -----');
+        logger.debug(JSON.stringify(context));
+    }
+
+    // Validate we have an Alexa directive
+    if (!('directive' in event)) {
+        let aer = new AlexaResponse(
+            {
+                'name': 'ErrorResponse',
+                'payload': {
+                    'type': 'INVALID_DIRECTIVE',
+                    'message': 'Missing key: directive, Is request a valid Alexa directive?'
+                }
+            });
+        return sendResponse(aer.get());
+    }
+
+    // Check the payload version
+    if (event.directive.header.payloadVersion !== '3') {
+        let aer = new AlexaResponse(
+            {
+                'name': 'ErrorResponse',
+                'payload': {
+                    'type': 'INTERNAL_ERROR',
+                    'message': 'This skill only supports Smart Home API version 3'
+                }
+            });
+        return sendResponse(aer.get());
+    }
+
+    let namespace = ((event.directive || {}).header || {}).namespace;
+
+    if (namespace.toLowerCase() === 'alexa.authorization') {
+        let aar = new AlexaResponse({'namespace': 'Alexa.Authorization', 'name': 'AcceptGrant.Response',});
+        return sendResponse(aar.get());
+    }
+
+    if (namespace.toLowerCase() === 'alexa.discovery') {
+        let adr = new AlexaResponse({'namespace': 'Alexa.Discovery', 'name': 'Discover.Response'});
+        let capability_alexa = adr.createPayloadEndpointCapability();
+        let capability_alexa_powercontroller = adr.createPayloadEndpointCapability({'interface': 'Alexa.PowerController', 'supported': [{'name': 'powerState'}]});
+        adr.addPayloadEndpoint({'friendlyName': 'Sample Switch', 'endpointId': 'sample-switch-01', 'capabilities': [capability_alexa, capability_alexa_powercontroller]});
+        return sendResponse(adr.get());
+    }
+};
+
+const sendResponse = (response) => {
+    // TODO Validate the response
+    logger.debug('index.handler response -----');
+    logger.debug(JSON.stringify(response));
+    return response;
+};
