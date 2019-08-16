e3ab7b991d3c92303a3fe3053dffb0e9e80bb2e9
Matt Turner <matt@fiverings.uk>
2019-08-16 21:40:58 +0100

    Retrieve profile

--- src/aws/smarthome-lambda.js
+++ src/aws/smarthome-lambda.js
@@ -6,6 +6,7 @@
 // const DefaultHoldStrategy = require('../core/HoldStrategy');
 // const ControlService = require('../core/ControlService');
 const Logger = require('../core/Logger');
+const axios = require('axios');
 // const helpers = require('./helpers');
 // const Factory = require('../thermostats/Factory');
 const AlexaResponse = require('./AlexaResponse');
@@ -49,17 +50,17 @@ exports.handler = async (event, context) => {
 
     let namespace = ((event.directive || {}).header || {}).namespace;
 
+    logger.debug('Retrieving profile data...');
+    let profile = await retrieveProfile(event);
+    
+
     if (namespace.toLowerCase() === 'alexa.authorization') {
         let aar = new AlexaResponse({'namespace': 'Alexa.Authorization', 'name': 'AcceptGrant.Response',});
         return sendResponse(aar.get());
     }
 
     if (namespace.toLowerCase() === 'alexa.discovery') {
-        let adr = new AlexaResponse({'namespace': 'Alexa.Discovery', 'name': 'Discover.Response'});
-        let capability_alexa = adr.createPayloadEndpointCapability();
-        let capability_alexa_powercontroller = adr.createPayloadEndpointCapability({'interface': 'Alexa.PowerController', 'supported': [{'name': 'powerState'}]});
-        adr.addPayloadEndpoint({'friendlyName': 'Sample Switch', 'endpointId': 'sample-switch-01', 'capabilities': [capability_alexa, capability_alexa_powercontroller]});
-        return sendResponse(adr.get());
+        return await handleDiscovery(profile, event);
     }
 };
 
@@ -68,4 +69,23 @@ const sendResponse = (response) => {
     logger.debug('index.handler response -----');
     logger.debug(JSON.stringify(response));
     return response;
+};
+
+const retrieveProfile = async (event) => {
+    const accessToken = event.directive.payload.scope.token;
+    const headers = {
+        Authorization: 'Bearer ' + accessToken,
+        'content-type': 'application/json'
+    };
+    const res = await axios.get('https://api.amazon.com/user/profile', { headers: headers });
+    logger.debug(JSON.stringify(res.data));
+    return res.data;
+};
+
+const handleDiscovery = async (profile, event) => {
+    let adr = new AlexaResponse({'namespace': 'Alexa.Discovery', 'name': 'Discover.Response'});
+    let capability_alexa = adr.createPayloadEndpointCapability();
+    let capability_alexa_powercontroller = adr.createPayloadEndpointCapability({'interface': 'Alexa.PowerController', 'supported': [{'name': 'powerState'}]});
+    adr.addPayloadEndpoint({'friendlyName': 'Sample Switch', 'endpointId': 'sample-switch-01', 'capabilities': [capability_alexa, capability_alexa_powercontroller]});
+    return sendResponse(adr.get());
 };
