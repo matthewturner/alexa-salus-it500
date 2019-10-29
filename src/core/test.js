0b18d0d412a85c345f72c4dfb4d65b96aae025ca
Matt Turner <matt@fiverings.uk>
2019-10-29 13:23:24 +0000

    Refactor test script

--- src/core/test.js
+++ src/core/test.js
@@ -31,24 +31,32 @@ const reportOn = async (action) => {
     }
 };
 
-const main = async () => {
-    const duration = process.env.DURATION;
-    const context = {
+const createHoldStrategy = (context) => {
+    if (process.env.HOLD_STRATEGY === 'aws') {
+        return new AwsHoldStrategy(logger, context);
+    }
+    return new DefaultHoldStrategy(logger, context);
+};
+
+const createRepository = () => {
+    if (process.env.THERMOSTAT_REPOSITORY === 'aws') {
+        return new AwsThermostatRepository(logger);
+    }
+    return new DefaultThermostatRepository(logger);
+};
+
+const createContext = () => {
+    return {
         userId: process.env.ALEXA_USER_ID,
         source: 'user'
     };
-    let holdStrategy;
-    if (process.env.HOLD_STRATEGY === 'aws') {
-        holdStrategy = new AwsHoldStrategy(logger, context);
-    } else {
-        holdStrategy = new DefaultHoldStrategy(logger, context);
-    }
-    let repository;
-    if (process.env.THERMOSTAT_REPOSITORY === 'aws') {
-        repository = new AwsThermostatRepository(logger);
-    } else {
-        repository = new DefaultThermostatRepository(logger);
-    }
+};
+
+const main = async () => {
+    const duration = process.env.DURATION;
+    const context = createContext();
+    let holdStrategy = createHoldStrategy(context);
+    let repository = createRepository();
     let setTemperatureStrategy = new SetTemperatureStrategy();
 
     const factory = new Factory(logger);
